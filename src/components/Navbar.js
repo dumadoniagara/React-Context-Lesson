611a353587b00ff7b7d9b149b23ce79bbf313ec4
dumadoniagara <dumadsambora@gmail.com>
2020-08-17 00:14:30 +0700

    lesson2/part2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,23 +3,25 @@ import { ThemeContext } from '../context/ThemeContext';
 
 
 class Navbar extends Component {
-  /* importing and make static ThemeContext not ThemeProvider component */
-  static contextType = ThemeContext; //attach state in themecontextprovider to this component
   render() {
-    // state in ThemeContextProvider what ever that pass in the value property  >> {isLight:true, ...}
-    const { isLightTheme, light, dark } = this.context;
-    console.log(this.context);
-    const theme = isLightTheme ? light : dark;
-
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          )
+        }}
+      </ThemeContext.Consumer>
+
     );
   }
 }
