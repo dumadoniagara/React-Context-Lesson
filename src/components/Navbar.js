e979d887652bd4b38cff4e1dc9cbdc59af464392
dumadoniagara <dumadsambora@gmail.com>
2020-08-17 01:38:15 +0700

    consume multiple context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
+import { AuthContext } from '../context/AuthContext';
+import { Switch } from 'antd';
+import 'antd/dist/antd.css';
 
 
 class Navbar extends Component {
   render() {
     return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <nav style={{ background: theme.ui, color: theme.syntax }}>
-              <h1>Context App</h1>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </nav>
-          )
-        }}
-      </ThemeContext.Consumer>
+      <AuthContext.Consumer>{(authContext) => (
+        <ThemeContext.Consumer>
+          {(themeContext) => {
+            const { isLightTheme, light, dark } = themeContext;
+            const { isAuthenticated, toggleAuth } = authContext;
+            const theme = isLightTheme ? light : dark;
+            return (
+              <nav style={{ background: theme.ui, color: theme.syntax }}>
+                <h1>Context App</h1>
+                <div style={{ marginBottom: '10px' }}>
+                  {isAuthenticated ? 'Logged in' : 'Logged out'}
+                  <span style={{ marginLeft: '10px' }}>
+                    <Switch checkedChildren="Login" unCheckedChildren="Logout" defaultChecked onChange={toggleAuth} />
+                  </span>
+                </div>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>
+            )
+          }}
+        </ThemeContext.Consumer>
+      )
+      }
+      </AuthContext.Consumer>
 
     );
   }
